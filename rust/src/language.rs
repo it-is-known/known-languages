4fd52bf76344338c6cbdb2f9f2c1a486bf60ccfa
Arto Bendiken <arto@bendiken.net>
2026-06-22 04:15:36 -0700

    Create the project.

---
+++ rust/src/language.rs
@@ -0,0 +1,46 @@
+// This is free and unencumbered software released into the public domain.
+
+use alloc::{fmt, string::String};
+use core::str::FromStr;
+
+/// An enumerated language.
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+pub enum Language {
+    English,
+    Other(String),
+}
+
+impl Language {
+    pub const ALL: &'static [Self] = &[
+        Self::English,
+        //Self::Other(String),
+    ];
+
+    pub fn as_str(&self) -> &str {
+        use Language::*;
+        match self {
+            English => "en",
+            Other(scheme) => scheme.as_str(),
+        }
+    }
+}
+
+impl FromStr for Language {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        use Language::*;
+        Ok(match input {
+            "en" => English,
+            scheme => Other(scheme.into()),
+        })
+    }
+}
+
+impl fmt::Display for Language {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
---
+++ rust/src/lib.rs
@@ -0,0 +1,23 @@
+// This is free and unencumbered software released into the public domain.
+
+//! This crate provides well-known languages.
+//!
+//! ```edition2024
+//! use known_languages::*;
+//! ```
+
+#![no_std]
+#![deny(unsafe_code)]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+mod language;
+pub use language::*;
+
+#[doc = include_str!("../../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
