eda44835ff0110299f178f5beb9b60dfe2f1c69c
Arto Bendiken <arto@bendiken.net>
2026-06-22 05:24:18 -0700

    Make the alloc feature optional.

--- rust/src/language.rs
+++ rust/src/language.rs
@@ -1,7 +1,9 @@
 // This is free and unencumbered software released into the public domain.
 
-use alloc::{fmt, string::String};
 use core::str::FromStr;
+
+#[cfg(feature = "alloc")]
+use alloc::string::String;
 
 /// An enumerated language.
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -15,21 +17,47 @@ use core::str::FromStr;
 pub enum Language {
     #[default]
     English,
+
+    #[cfg(feature = "alloc")]
     Other(String),
 }
 
 impl Language {
-    pub const ALL: &'static [Self] = &[
-        Self::English,
-        //Self::Other(String),
-    ];
+    pub const ALL: &'static [Self] = &[Self::English];
 
     pub fn as_str(&self) -> &str {
         use Language::*;
         match self {
             English => "en",
-            Other(scheme) => scheme.as_str(),
+
+            #[cfg(feature = "alloc")]
+            Other(input) => input.as_str(),
         }
+    }
+
+    #[cfg(feature = "alloc")]
+    pub fn into_string(self) -> String {
+        self.as_str().into()
+    }
+
+    #[cfg(all(feature = "serde", feature = "alloc"))]
+    pub fn to_json(&self) -> Option<serde_json::Value> {
+        Some(self.clone().into_json())
+    }
+
+    #[cfg(all(feature = "serde", feature = "alloc"))]
+    pub fn into_json(self) -> serde_json::Value {
+        serde_json::Value::String(self.into_string())
+    }
+
+    #[cfg(all(feature = "bson", feature = "alloc"))]
+    pub fn to_bson(&self) -> Option<bson::Bson> {
+        Some(self.clone().into_bson())
+    }
+
+    #[cfg(all(feature = "bson", feature = "alloc"))]
+    pub fn into_bson(self) -> bson::Bson {
+        bson::Bson::String(self.into_string())
     }
 }
 
@@ -40,18 +68,23 @@ impl FromStr for Language {
         use Language::*;
         Ok(match input {
             "en" => English,
-            scheme => Other(scheme.into()),
+
+            #[cfg(feature = "alloc")]
+            input => Other(input.into()),
+
+            #[cfg(not(feature = "alloc"))]
+            _ => return Err(()),
         })
     }
 }
 
-impl fmt::Display for Language {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl core::fmt::Display for Language {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{}", self.as_str())
     }
 }
 
-#[cfg(feature = "alloc")]
+#[cfg(all(feature = "serde", feature = "alloc"))]
 impl TryFrom<serde_json::Value> for Language {
     type Error = ();
 
