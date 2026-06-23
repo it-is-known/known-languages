7332905c2f67d94463f512cd1b19263e17f63a71
Arto Bendiken <arto@bendiken.net>
2026-06-23 00:42:37 -0700

    Create the Dart package.

---
+++ dart/test/known_languages_test.dart
@@ -0,0 +1,6 @@
+// This is free and unencumbered software released into the public domain.
+
+import 'package:known_languages/known_languages.dart';
+import 'package:test/test.dart';
+
+void main() {}
